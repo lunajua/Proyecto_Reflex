f02ac87f6848acceeea6d5cf752af50a916fc5e4
Juan Pablo Luna <jp_luna@hotmail.com>
2024-04-28 16:13:58 -0300

    Hola Mundo

--- my_web/my_web/my_web.py
+++ my_web/my_web/my_web.py
@@ -1,14 +1,18 @@
-"""Welcome to Reflex!."""
-
-# Import all the pages.
-from my_web.pages import *
-
 import reflex as rx
 
-
+#Creamos una clase que hereda de rx.State. Esta clase en un futuro crear aplicaciones interactivas.
 class State(rx.State):
-    """Define empty state to allow access to rx.State.router."""
+    pass
 
+#Creamos una función que tiene que devolver un componente para representar la parte gráfica.
+def index() -> rx.Component: 
+    return rx.text("Hola Reflex!", color="red")
 
-# Create the app.
+#Para ejecutar el programa, tenemos que crear una instancia de rx.App.
 app = rx.App()
+app.add_page(index) #Mediante este método agregamos por ahora la única página que tenemos que es nuestro index.
+app.compile() #Ejecutamos el programa.    
+
+
+    
+
